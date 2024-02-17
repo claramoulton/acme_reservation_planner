ef1b107c5ea8882b376e5fdb09e665eac64bbf67
EC2 Default User <ec2-user@ip-172-31-39-202.us-east-2.compute.internal>
2024-02-17 13:35:04 +0000

    tables created

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-console.log('hola el mundo');
+const db = require('./db');
+const client = db.client;
+const createTables = db.createTables;
+
+const init = async()=> {
+    console.log('connecting to database');
+    await client.connect();
+    console.log('connected to dataase');
+    await createTables();
+    console.log('tables created');
+};
+
+init();
