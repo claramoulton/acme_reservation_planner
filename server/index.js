6cd42354ee5f0b838cf35b53623a2e6e6e34bd46
EC2 Default User <ec2-user@ip-172-31-39-202.us-east-2.compute.internal>
2024-02-20 12:37:54 +0000

    lucy vacation

--- server/index.js
+++ server/index.js
@@ -8,6 +8,35 @@ const fetchPlaces = db.fetchPlaces;
 const createVacation = db.createVacation;
 const fetchVacations = db.fetchVacations;
 const destroyVacation = db.destroyVacation;
+const express = require('express');
+const app = express();
+
+app.get('/api/users', async(req, res, next)=> {
+    try {
+        res.send(await fetchUsers());
+    }
+    catch(ex){
+        next(ex);
+    }
+});
+
+app.get('/api/places', async(req, res, next)=> {
+    try {
+        res.send(await fetchPlaces());
+    }
+    catch(ex){
+        next(ex);
+    }
+});
+
+app.get('/api/users/:id/vacations', async(req, res, next)=> {
+    try {
+        res.send(await fetchVacations(req.params.id));
+    }
+    catch(ex){
+        next(ex);
+    }
+});
 
 const init = async()=> {
     console.log('connecting to database');
@@ -26,14 +55,27 @@ const init = async()=> {
     ]);
     console.log(await fetchUsers());
     console.log(await fetchPlaces());
-    const vacation = await createVacation({
+    let vacation = await createVacation({
         departure_date: '03/15/2024',
         place_id: dallas.id,
+        user_id: lucy.id
+    });
+    vacation = await createVacation({
+        departure_date: '03/19/2024',
+        place_id: paris.id,
         user_id: lucy.id
     });
     console.log(await fetchVacations(lucy.id));
     await destroyVacation(vacation);
     console.log(await fetchVacations(lucy.id));
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=>{
+        console.log(`listening on port ${port}`);
+        console.log(`curl localhost:${port}/api/users`);
+        console.log(`curl localhost:${port}/api/places`);
+        console.log(`curl localhost:${port}/api/users/${lucy.id}/vacations`);
+
+    });
 };
 
 
